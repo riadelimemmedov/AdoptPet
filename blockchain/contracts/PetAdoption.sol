04b56fbca4598716b449f1c747b465eb61e674eb
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2024-01-27 23:12:40 +0400

    Writed add pet test if deployer and buyer not match

--- blockchain/contracts/PetAdoption.sol
+++ blockchain/contracts/PetAdoption.sol
@@ -3,12 +3,21 @@ pragma solidity ^0.8.9;
 
 contract PetAdoption {
     address public owner;
+    uint public petIndex = 0;
 
     constructor() {
         owner = msg.sender;
     }
 
-    function getOwner() public view returns(address) {
+    function addPet() public {
+        require(
+            owner == msg.sender,
+            "Only a contract owner can be add a new pet!"
+        );
+        petIndex++;
+    }
+
+    function getOwner() public view returns (address) {
         return owner;
     }
 }
--- blockchain/test/PetAdoption.test.js
+++ blockchain/test/PetAdoption.test.js
@@ -10,11 +10,12 @@ describe("PetAdoption", function () {
 
 	async function deployContractFixture() {
 		// eslint-disable-next-line no-undef
-		const [deployer] = await hre.ethers.getSigners()
+		const [deployer,account2] = await hre.ethers.getSigners()
 		const contract = await (await ethers.getContractFactory("PetAdoption")).deploy()
 		const randomNumber = Math.random()
 		return {
 			deployer,
+			account2,
 			contract,
 			randomNumber
 		}
@@ -34,6 +35,13 @@ describe("PetAdoption", function () {
 		it("should have a non-null contract address", async function () {
 			const {contract} = await loadFixture(deployContractFixture)
 			expect(contract.address).to.not.be.null
+		})
+	})
+
+	describe("Add Pet",function () {
+		it("should revert with the right error in case of other account",async function () {
+			const {account2,contract} = await loadFixture(deployContractFixture)
+			await expect(contract.connect(account2).addPet()).to.be.revertedWith("Only a contract owner can be add a new pet!")//Error text must be match to require message text on smart contract
 		})
 	})
 })
