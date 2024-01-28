a4bd856c5fe0109fc536a6950a07a2690321d0b9
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2024-01-29 02:40:38 +0400

    Deploy frontend on docker

---
+++ frontend/contract-ui/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  //*If you have deploy application on docker comment out this area
+  server: {
+    host: true,
+    port: 8000, // This is the port which we will use in docker
+    watch: {
+      usePolling: true
+    }
+  }
+})
