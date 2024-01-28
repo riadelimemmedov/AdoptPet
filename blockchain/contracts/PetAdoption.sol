0a16fc31a01f5e362060bd22ae4125cf51cb235b
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2024-01-28 13:47:40 +0400

    Adding solidity code coverage for tests and writing some tests related to adopt pet functionality

--- blockchain/contracts/PetAdoption.sol
+++ blockchain/contracts/PetAdoption.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.9;
 contract PetAdoption {
     address public owner;
     uint public petIndex = 0;
+    uint[] allAdoptedPets;
+
+    mapping(uint => address) public petIdxToOwnerAddress;
+    mapping(address => uint[]) public ownerAddressToPetList;
 
     constructor(uint initialPetIndex) {
         owner = msg.sender;
@@ -11,11 +15,17 @@ contract PetAdoption {
     }
 
     function addPet() public {
-        require(
-            owner == msg.sender,
-            "Only a contract owner can be add a new pet!"
-        );
+        require(owner == msg.sender,"Only a contract owner can be add a new pet!");
         petIndex++;
+    }
+
+    function adoptPet(uint adoptIdx) public {
+        require(adoptIdx < petIndex,"Pet index out of the bounds!");
+        require(petIdxToOwnerAddress[adoptIdx] == address(0),"Pet is already adopted!");
+
+        petIdxToOwnerAddress[adoptIdx] = msg.sender;
+        ownerAddressToPetList[msg.sender].push(adoptIdx);
+        allAdoptedPets.push(adoptIdx);
     }
 
     function getOwner() public view returns (address) {
--- blockchain/test/PetAdoption.test.js
+++ blockchain/test/PetAdoption.test.js
@@ -11,13 +11,18 @@ describe("PetAdoption", function () {
 	async function deployContractFixture() {
 		// eslint-disable-next-line no-undef
 		const PETS_COUNT = 5
-		const [deployer,account2] = await hre.ethers.getSigners()
+		const ADOPTED_PED_IDX = 0
+		const [deployer,account2,account3] = await hre.ethers.getSigners()
 		const contract = await (await ethers.getContractFactory("PetAdoption")).deploy(PETS_COUNT)
+
+		await contract.connect(account3).adoptPet(ADOPTED_PED_IDX)
+
 		return {
 			deployer,
 			account2,
 			contract,
-			petsAddedCount:PETS_COUNT
+			petsAddedCount:PETS_COUNT,
+			adoptedPetIdx:ADOPTED_PED_IDX
 		}
 	}
 
@@ -47,6 +52,18 @@ describe("PetAdoption", function () {
 			const {petsAddedCount,contract} = await loadFixture(deployContractFixture)
 			await contract.addPet()
 			expect(await contract.petIndex()).to.equal(petsAddedCount+1)//I am wait 6
+		})
+	})
+
+	describe("Adopt Pet",function () {
+		it("should revert with index out of bounds", async function () {
+			const {contract,petsAddedCount} = await loadFixture(deployContractFixture)
+			await expect(contract.adoptPet(petsAddedCount)).to.be.revertedWith("Pet index out of the bounds!")
+			await expect(contract.adoptPet(-1)).to.be.rejectedWith("value out-of-bounds")
+		})
+		it("should revert with the pet is already adopted", async function () {
+			const {contract,adoptedPetIdx} = await loadFixture(deployContractFixture)
+			await expect(contract.adoptPet(adoptedPetIdx)).to.be.revertedWith("Pet is already adopted!")
 		})
 	})
 })
